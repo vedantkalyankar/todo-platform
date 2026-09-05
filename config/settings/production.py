5987399d183babe615335645525e0de62d86ee93
Vedant Kalyankar <vedantkalyankar777@gmail.com>
2026-09-06 01:42:36 +0530

    feat: Added https from nginx

--- config/settings/production.py
+++ config/settings/production.py
@@ -40,9 +40,13 @@ MAILERS = {
         ),
     },
 }
-
-
 SECURE_SSL_REDIRECT = True
+
+SECURE_PROXY_SSL_HEADER = (
+    "HTTP_X_FORWARDED_PROTO",
+    "https",
+)
+
 
 SESSION_COOKIE_SECURE = True
 
